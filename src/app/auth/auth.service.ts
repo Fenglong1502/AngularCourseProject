00309f714dd5252b7e29a9dd4150fb31f09e42a8
fenglong_ng <fenglong1502@hotmail.com>
2021-02-28 23:01:50 +0800

    Implemented features module

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService{
     constructor(private http: HttpClient, private router: Router){}
     
     signup(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB__t-c3mnP_7cw8xw1pHQ8iwUGiMQUgvY',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
         {
             email: email,
             password: password,
@@ -35,7 +35,7 @@ export class AuthService{
     }
     
     login(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB__t-c3mnP_7cw8xw1pHQ8iwUGiMQUgvY', {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=', {
             email: email,
             password: password, 
             returnSecureToken: true
