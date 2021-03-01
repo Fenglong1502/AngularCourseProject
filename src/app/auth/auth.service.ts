63b576bbe35fc38e1b915b55234f703d8e91bc9a
fenglong_ng <fenglong1502@hotmail.com>
2021-03-01 21:59:20 +0800

    added environment for deployment

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, Subject, throwError } from 'rxjs';
 import { User } from "./user.model";
 import { Router } from "@angular/router";
+import { environment } from '../../environments/environment'
 
 export interface AuthResponseData{
     idToken: string;
@@ -22,7 +23,7 @@ export class AuthService{
     constructor(private http: HttpClient, private router: Router){}
     
     signup(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
@@ -35,7 +36,7 @@ export class AuthService{
     }
     
     login(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=', {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password, 
             returnSecureToken: true
