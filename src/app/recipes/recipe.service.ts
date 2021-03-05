89b4dfc42cf858774eaba537477d9d6cd56f5f82
fenglong_ng <fenglong1502@hotmail.com>
2021-03-05 23:57:25 +0800

    implemented store

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 
 @Injectable()
 export class RecipeService {
@@ -27,41 +29,44 @@ export class RecipeService {
 
     private recipes: Recipe[] = [];
 
-    constructor(private slService: ShoppingListService){
-
+    constructor(
+        private store: Store<fromShoppingList.AppState>
+    ) {
+        
     }
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice(); // To not give the reference
     }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]){
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
         // ingredients.map(ingredient => {
         //     this.slService.addIngredient(ingredient);
         // })
-        this.slService.addIngredients(ingredients);
+        // this.slService.addIngredients(ingredients);
+        this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients))
     }
-    
-    addRecipe(recipe: Recipe){
+
+    addRecipe(recipe: Recipe) {
         this.recipes.push(recipe)
         this.recipeChanged.next(this.recipes.slice());
     }
 
-    updateRecipe(index : number, newRecipe: Recipe){
+    updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
         this.recipeChanged.next(this.recipes.slice());
     }
 
-    getRecipe(index: number){
+    getRecipe(index: number) {
         return this.recipes[index];
     }
-    
-    deleteRecipe(index: number){
+
+    deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
         this.recipeChanged.next(this.recipes.slice());
     }
 
-    setRecipes(recipes: Recipe[]){
+    setRecipes(recipes: Recipe[]) {
         this.recipes = recipes;
         this.recipeChanged.next(this.recipes.slice());
     }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
-import { LoggingService } from '../logging.service';
+
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
+import { LoggingService } from '../logging.service';
+import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -16,9 +18,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   private subscription : Subscription;
 
   constructor(
-    private slService: ShoppingListService,
     private loggingService: LoggingService,
-    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    private store: Store<fromShoppingList.AppState>
   ) { }
 
   ngOnInit(): void {
@@ -32,11 +33,12 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.igChangeSub.unsubscribe();
+    // this.igChangeSub.unsubscribe();
   }
 
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    // this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 }
